981d421c1083a00b63015362e47f6bfec10947ba
Oliver Gorwits <oliver@cpan.org>
2008-05-01 13:56:21 +0000

    rt.cpan#34680

--- lib/Net/MAC.pm
+++ lib/Net/MAC.pm
@@ -30,7 +30,7 @@ use overload
 
 # RCS ident string
 #my $rcs_id = '$Id$';
-our $VERSION = '1.3';
+our $VERSION = '1.4';
 our $AUTOLOAD;
 
 # Constructor.
@@ -418,7 +418,7 @@ sub convert {
     my ( $self, %arg ) = @_;
     my $imac = $self->get_internal_mac();
     my @groups;
-    my $bit_group = $arg{'bit_group'} || 8;    # FIXME: ?
+    my $bit_group = $arg{'bit_group'} || 48; # default value
     my $offset = 0;
     use integer;
     my $size = $bit_group / 4;
@@ -456,6 +456,10 @@ sub convert {
 
         #warn "\nconvert groups @groups\n";
     }
+    elsif ($bit_group != 48) {
+        # use default delimiter
+        $mac_string = join( ':', @groups );
+    } 
     else {
         $mac_string = join( '', @groups );
     }
@@ -709,6 +713,13 @@ or the numeric base.
           'bit_group' => 16,    # 16 bit grouping
           'delimiter' => '.'    # dot-delimited
   );
+
+Note that if any of the above arguments are not provided, they will be set to
+the following default values:
+
+ base       16
+ bit_group  48  (i.e. no delimiter)
+ delimiter  :
 
 =head2 Conversion to common formats
 
